aa797748ea6a286902b5721e2a19bec84eab47ad
Alysson Prado Maynart <114994563+AlyssonPMaynart@users.noreply.github.com>
2024-11-04 22:05:23 -0300

    Revisão e correção

--- ex036.py
+++ ex036.py
@@ -3,8 +3,8 @@ print('ANÁLISE DE EMPRÉSTIMO BANCÁRIO')
 print('-='*15)
 imovel = float(input("Informe o valor do imóvel desejado: R$ "))
 salario = float(input("Informe o valor de sua remuneração: R$ "))
-parcelas = float(input("Informe em quantos anos pretende quitar o empréstimo: R$ "))
-prestação = imovel / (parcelas * 12)
+anos = int(input("Informe em quantos anos pretende quitar o empréstimo: R$ "))
+prestação = imovel / (anos * 12)
 if prestação <= salario * 0.3:
     print('Parabéns, seu emprestimo foi aprovado!')
 else:
--- ex037.py
+++ ex037.py
@@ -8,10 +8,10 @@ conv = int(input("Indique a base que deseja converter o número \n"
       "[3] converter para HEXADECIMAL\n"
       "Sua Opção: "))
 if conv== 1:
-    print(f"{num} convertido para BINÁRIO é igual a {bin(num)[2:]}")
+    print('{} convertido para BINÁRIO é igual a {}'.format(bin(num)[2:]))
 elif conv == 2:
-    print(f"{num} convertido para OCTAL é igual a {oct(num)[2:]}")
+    print('{} convertido para OCTAL é igual a {}'.format(oct(num)[2:]))
 elif conv == 3:
-    print(f"{num} convertio para HEXADECIMAL é igual a {hex(num)[2:]}")
+    print('{} convertio para HEXADECIMAL é igual a {}'.format(hex(num)[2:]))
 else:
     print("Você é burro? Opção inválida")
