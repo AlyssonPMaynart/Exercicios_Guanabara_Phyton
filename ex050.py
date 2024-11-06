081ccd1068510605964ae718821df1ef6e3e37d4
Alysson Prado Maynart <114994563+AlyssonPMaynart@users.noreply.github.com>
2024-11-06 20:46:42 -0300

    Revisão e correção

--- ex050.py
+++ ex050.py
@@ -1,6 +1,8 @@
+# Soma de números inteiros pares, apartir do imput do usuário.
+
 soma = 0
 for i in range(1,7):
-    num = int(input('Digite um numero interiro: '))
+    num = int(input('Digite um numero inteiro: '))
     if num % 2 == 0:
         soma += num
 print('A soma de todos os numero pares é igual a {}'.format(soma))
